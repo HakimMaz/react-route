99f3befada9dd7a90811bf8b98c1dd6a2ef0594c
HakimMaz <maz.hakim93@gmail.com>
2020-04-20 17:57:28 +0100

    using link tag to prevent default request to server and refreshing page

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -9,9 +9,9 @@ const Navbar = () => {
         <ul className="right">
 
          
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-          <li><NavLink to="/contact">Contact</NavLink></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
           
         </ul>
       </div>
