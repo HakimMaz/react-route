07f9ba46301a90e1a7a07c4102f3f54c5fe6663f
HakimMaz <maz.hakim93@gmail.com>
2020-04-24 17:14:29 +0100

    using switch to load one url in moment

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import Navbar from'./Component/Navbar';
-import {BrowserRouter,Route} from 'react-router-dom';
-import Home from './Component/Home';
-import About from './Component/About';
-import Contact from './Component/Contact';
-import Post from './Component/Post';
+import React from "react";
+import Navbar from "./Component/Navbar";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Component/Home";
+import About from "./Component/About";
+import Contact from "./Component/Contact";
+import Post from "./Component/Post";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Route exact path='/' component={Home}/>
-        <Route path='/about' component={About}/>
-        <Route path='/Contact' component={Contact}/>
-        <Route path="/:post_id" component={Post}/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/Contact" component={Contact} />
+          <Route path="/:post_id" component={Post} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
