6de280fa84aca6606efd121162bd615a504a724e
HakimMaz <maz.hakim93@gmail.com>
2020-04-24 17:37:24 +0100

    adding images

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import {Link} from 'react-router-dom';
+import Pokeball from '../pokeball.png';
 class Home extends Component {
   state = {
     posts: [],
@@ -20,9 +21,10 @@ class Home extends Component {
       posts.map((post) => {
         return (
           <div className="post card" key={post.id}>
+           <img src={Pokeball} alt="A pokeball"/>
             <div className="card-content">
              <Link to={'/'+post.id}>
-              <span className="card-title">{post.title}</span>
+              <span className="card-title red-text">{post.title}</span>
              </Link>
               <p>{post.body}</p>
             </div>
@@ -33,7 +35,7 @@ class Home extends Component {
       <div className="center">No posts to display</div>
     );
     return (
-      <div className="container">
+      <div className="container home">
         <h4 className="center">Home</h4>
          {postList}
       </div>
