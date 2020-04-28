d907cd18da6afda49a8c5dcdf99b1f3e33f5eaaf
HakimMaz <maz.hakim93@gmail.com>
2020-04-28 18:36:57 +0100

    using redux, and updating home component to connect to redux

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,22 +1,10 @@
 import React, { Component } from "react";
-import axios from "axios";
 import {Link} from 'react-router-dom';
 import Pokeball from '../pokeball.png';
+import {connect} from 'react-redux';
 class Home extends Component {
-  state = {
-    posts: [],
-  };
-  componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
-      console.log("result of request ", res);
-      this.setState({
-        posts: res.data.slice(0, 10),
-      });
-    });
-  }
-
   render() {
-    const { posts } = this.state;
+    const { posts } = this.props;
     const postList = posts.length ? (
       posts.map((post) => {
         return (
@@ -42,4 +30,9 @@ class Home extends Component {
     );
   }
 }
-export default Home;
+const mapsStateToProps=(state)=>{
+  return{
+    posts:state.posts
+  }
+}
+export default connect(mapsStateToProps)(Home);
